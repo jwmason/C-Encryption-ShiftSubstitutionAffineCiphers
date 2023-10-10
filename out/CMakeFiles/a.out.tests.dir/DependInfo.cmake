c157776e4f780994cce162bc03d6eb34d5744848
Mason Wong <masonjw1@uci.edu>
2023-10-09 21:56:25 -0700

    added .vscode directory and subfiles tasks.json and launch.json. Also came with a main subfile in app directory and an out directory

---
+++ out/CMakeFiles/a.out.tests.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/masonjw1/project0/app/ciphers.cpp" "CMakeFiles/a.out.tests.dir/app/ciphers.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/app/ciphers.cpp.o.d"
+  "/home/masonjw1/project0/tests/AffineTests.cpp" "CMakeFiles/a.out.tests.dir/tests/AffineTests.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/tests/AffineTests.cpp.o.d"
+  "/home/masonjw1/project0/tests/ShiftTests.cpp" "CMakeFiles/a.out.tests.dir/tests/ShiftTests.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/tests/ShiftTests.cpp.o.d"
+  "/home/masonjw1/project0/tests/SubstituteTests.cpp" "CMakeFiles/a.out.tests.dir/tests/SubstituteTests.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/tests/SubstituteTests.cpp.o.d"
+  "/home/masonjw1/project0/tests/catch_amalgamated.cpp" "CMakeFiles/a.out.tests.dir/tests/catch_amalgamated.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/tests/catch_amalgamated.cpp.o.d"
+  "/home/masonjw1/project0/tests/main.cpp" "CMakeFiles/a.out.tests.dir/tests/main.cpp.o" "gcc" "CMakeFiles/a.out.tests.dir/tests/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/CMakeFiles/a.out.tests.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.tests.dir/app/ciphers.cpp.o"
+  "CMakeFiles/a.out.tests.dir/app/ciphers.cpp.o.d"
+  "CMakeFiles/a.out.tests.dir/tests/AffineTests.cpp.o"
+  "CMakeFiles/a.out.tests.dir/tests/AffineTests.cpp.o.d"
+  "CMakeFiles/a.out.tests.dir/tests/ShiftTests.cpp.o"
+  "CMakeFiles/a.out.tests.dir/tests/ShiftTests.cpp.o.d"
+  "CMakeFiles/a.out.tests.dir/tests/SubstituteTests.cpp.o"
+  "CMakeFiles/a.out.tests.dir/tests/SubstituteTests.cpp.o.d"
+  "CMakeFiles/a.out.tests.dir/tests/catch_amalgamated.cpp.o"
+  "CMakeFiles/a.out.tests.dir/tests/catch_amalgamated.cpp.o.d"
+  "CMakeFiles/a.out.tests.dir/tests/main.cpp.o"
+  "CMakeFiles/a.out.tests.dir/tests/main.cpp.o.d"
+  "bin/a.out.tests"
+  "bin/a.out.tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
